fafd80cc6742240997e3e98dc27e9c465668f28d
Gregory Wong <K444.G4x07@gmail.com>
2016-04-13 10:24:05 +0800

    Added new function to read weather data

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,31 @@
 $(function(){
     var shoesData = [{name:"Nike", price:199.00 }, {name:"Loafers", price:59.00 }, {name:"Wing Tip", price:259.00 }];
 
-    //var items;
-  //  $.getJSON("SomeData.json", function (data) {
-//	items = data;
-
-    //});
-var items = (function () {
     var items = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': "./SomeData.json",
-        'dataType': "json",
-        'success': function (data) {
-            items = data;
-        }
-    });
-    return items;
-})(); 
 
     //Get the HTML from the template   in the script tag
     var theTemplateScript = jQuery("#shoe-template").html(); 
     
    //Compile the template
     var theTemplate = Handlebars.compile (theTemplateScript); 
-    $(".shoesNav").append (theTemplate(items));
+    $(".shoesNav").append (theTemplate(shoesData));
 });
+
+$(function(){
+    var xhReq = new XMLHttpRequest();
+    xhReq.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Singapore");
+    xhReq.addEventListener("readystatechange", function(){
+	if(xhReq.readyState ==4 && xhReq.status == 200){
+	    var jsonObject = JSON.parse(xhReq.responseText);
+	    jsonObject["main"].temp = Math.round(jsonObject["main"].temp-273) // covert K to C
+	    var weatherDataScript = jQuery("#weather-data").html(); 
+	    var weatherData = Handlebars.compile (weatherDataScript); 
+	    $(".weather").append (weatherData(jsonObject));
+
+	}
+    });
+    xhReq.send();
+})
 
 function documentGetElement(id){
     return document.getElementById(id);
